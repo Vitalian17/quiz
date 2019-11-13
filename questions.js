facbd35fa5a0cb9def7f7a66337a6932d9f6e533
Tony Sarni <sarni.tony@gmail.com>
2019-11-12 19:59:58 -0600

    redid almost everything and still not happy with it at all but at least it does something

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Which of the following is NOT a professional Pittsburgh sports team?",
+        answers: ["Penguins", "Pirates", "Steelers", "Parrots"],
+        answer: "Parrots"
+    },
+    {
+        title: "Which Penguins player wears number 87?",
+        answers: ["Kris Letang", "Matt Murray", "Sidney Crosby", "Adam Banks"],
+        answer: "Sidney Crosby"
+    },
+    {
+        title: "How many Supebowls have the Pittsburgh Steelers won?",
+        answers: ["6", "4", "2", "none"],
+        answer: "6"
+    },
+    {
+        title: "Where did quarterback Ben Rothlisberger attend college?",
+        answers: ["UCLA", "Michigan", "Miami", "Ohio State"],
+        answer: "Miami"
+    },
+    {
+        title: "What NFL team do Steelers fans despise above all others?",
+        answers: ["Cleveland Browns", "Cincinnati Bengals", "Tennessee Titans", "Baltimore Ravens"],
+        answer: "Baltimore Ravens"
+    },
+]
---
+++ quiz.js
@@ -0,0 +1,120 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+
+var questions = [
+    new Question("Which of the following is NOT a professional Pittsburgh sports team?", ["Penguins", "Pirates", "Steelers", "Parrots"], "Parrots"),
+    new Question("Which Penguins player wears number 87?", ["Kris Letang", "Matt Murray", "Sidney Crosby", "Adam Banks"], "Sidney Crosby"),
+    new Question("How many Supebowls have the Pittsburgh Steelers won?", ["6", "4", "2", "none"], "6"),
+    new Question("Where did quarterback Ben Rothlisberger attend college?", ["UCLA", "Michigan", "Miami", "Ohio State"], "Miami"),
+    new Question("What NFL team do Steelers fans despise above all others?", ["Cleveland Browns", "Cincinnati Bengals", "Tennessee Titans", "Baltimore Ravens"], "Baltimore Ravens")
+];
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var twoMinutes = 60 * 2,
+        display = document.querySelector('#time');
+    startTimer(twoMinutes, display);
+};
+ 
+var timer = document.getElementById('timer');
+
+function gameOver() {
+    if (timer <= 0) {
+        alert("Game Over");
+    }
+}
+var quiz = new Quiz(questions);
+ 
+
+populate();
